ecc50a62768cc5ee640cb78e8bab3121a5a361f1
Ruslan Galiullin <160370317+kjetteren@users.noreply.github.com>
2025-07-03 23:46:11 +0200

    Added automatic role selection for announcements

--- src/modal.rs
+++ src/modal.rs
@@ -14,16 +14,36 @@ struct MyModal {
     #[placeholder = "HH:MM"]
     time: String,
 }
+
 #[poise::command(slash_command)]
 pub async fn announce(ctx: ApplicationContext<'_>) -> Result<(), Error> {
-    let data = MyModal::execute(ctx).await?.unwrap();
-    println!("Got data: {:?}", data);
-    ctx.say(format!(
-        "Announcement created for **{}** on {} at {}",
-        data.title, data.date, data.time
-    ))
-        .await
-        .map_err(Error::from)?;
+    let mut role_id = None;
+
+    if let Some(guild_id) = ctx.guild_id() {
+        if let Some(guild) = guild_id.to_guild_cached(&ctx) {
+            role_id = guild
+                .role_by_name("Kino Enjoyers")
+                .map(|role| role.id);
+        }
+    }
+
+    if let Some(data) = MyModal::execute(ctx).await? {
+        if let Some(role_id) = role_id {
+            let content = poise::serenity_prelude::utils::MessageBuilder::new()
+                .role(role_id)
+                .push(", ")
+                .push(data.title)
+                .push(": on ")
+                .push(data.date)
+                .push(" at ")
+                .push(data.time)
+                .build();
+
+            ctx.say(content)
+                .await
+                .map_err(Error::from)?;
+        }
+    }
 
     Ok(())
 }
